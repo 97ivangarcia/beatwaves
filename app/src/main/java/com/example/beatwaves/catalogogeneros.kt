42271942c4bd10f8bd9c065c13610da80df55dd5
4ndreaCatMen <catalanmenachoandrea@gmail.com>
2024-11-18 19:40:09 +0100

    barra con iconos y perfil

--- app/src/main/java/com/example/beatwaves/catalogogeneros.kt
+++ app/src/main/java/com/example/beatwaves/catalogogeneros.kt
@@ -1,21 +1,46 @@
 package com.example.beatwaves
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class catalogogeneros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val rodri="funciona"
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_catalogogeneros)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Obtener la referencia al ConstraintLayout principal
+        val mainLayout: ConstraintLayout = findViewById(R.id.main)
+
+        // Ajustar el padding de la vista principal según las insets del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+            // Obtener los márgenes de las insets de las barras del sistema (barra de estado y barra de navegación)
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // Establecer el padding superior, izquierdo, derecho e inferior
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            // Devolver las insets para que se apliquen correctamente
             insets
         }
+
+
+        val botonusuario = findViewById<ImageButton>(R.id.navigation_user);
+
+
+
+        botonusuario.setOnClickListener{
+            val intent = Intent(this@catalogogeneros, perfiluser::class.java)
+            startActivity(intent)
+        }
+
     }
 }
