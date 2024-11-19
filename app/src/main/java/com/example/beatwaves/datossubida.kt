e026867f0f9075718c8a962068746cd1252c68f0
ivan <97ivangarcia@gmail.com>
2024-11-19 17:46:55 +0100

    funcion botones

--- app/src/main/java/com/example/beatwaves/datossubida.kt
+++ app/src/main/java/com/example/beatwaves/datossubida.kt
@@ -1,37 +1,20 @@
 package com.example.beatwaves
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class subircancion : AppCompatActivity() {
+class datossubida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_subircancion)
+        setContentView(R.layout.activity_datossubida)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val perfil = findViewById<ImageButton>(R.id.botonperfil);
-        val home = findViewById<ImageButton>(R.id.botoncatalogo);
-
-        perfil.setOnClickListener {
-            val intent = Intent(this@subircancion, perfiluser::class.java)
-            startActivity(intent)
-
-
-        }
-        home.setOnClickListener {
-            val intent = Intent(this@subircancion, catalogogeneros::class.java)
-            startActivity(intent)
-
-
         }
     }
 }
--- app/src/main/java/com/example/beatwaves/perfiluser.kt
+++ app/src/main/java/com/example/beatwaves/perfiluser.kt
@@ -1,6 +1,8 @@
 package com.example.beatwaves
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,21 @@ class perfiluser : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val home = findViewById<ImageButton>(R.id.perfiluserGoHome);
+        val subir = findViewById<ImageButton>(R.id.perfiluserGoSubir);
+
+        home.setOnClickListener {
+            val intent = Intent(this@perfiluser, catalogogeneros::class.java)
+            startActivity(intent)
+
+
+        }
+        subir.setOnClickListener {
+            val intent = Intent(this@perfiluser, subircancion::class.java)
+            startActivity(intent)
+
+
         }
     }
 }
