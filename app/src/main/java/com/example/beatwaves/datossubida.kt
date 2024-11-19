9846927fd53fd6c57f34f9f81bc5506f15587dde
ivan <97ivangarcia@gmail.com>
2024-11-19 18:35:47 +0100

    pantalla de datos para subir la cancion

--- app/src/main/java/com/example/beatwaves/datossubida.kt
+++ app/src/main/java/com/example/beatwaves/datossubida.kt
@@ -1,6 +1,9 @@
 package com.example.beatwaves
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,15 @@ class datossubida : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val volver = findViewById<Button>(R.id.subirvolver);
+
+        volver.setOnClickListener {
+            val intent = Intent(this@datossubida, subircancion::class.java)
+            startActivity(intent)
+
+
         }
     }
 }
