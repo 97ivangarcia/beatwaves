8070855025f8bc897d677e119e7119dbd42e18e0
ivan <97ivangarcia@gmail.com>
2024-11-19 19:36:02 +0100

    pantalla de editar datos usuario

--- app/src/main/java/com/example/beatwaves/tarjeta.kt
+++ app/src/main/java/com/example/beatwaves/tarjeta.kt
@@ -2,42 +2,40 @@ package com.example.beatwaves
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class perfiluser : AppCompatActivity() {
+class tarjeta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_perfiluser)
+        setContentView(R.layout.activity_tarjeta)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val home = findViewById<ImageButton>(R.id.perfiluserGoHome);
-        val subir = findViewById<ImageButton>(R.id.perfiluserGoSubir);
-        val editperfil = findViewById<Button>(R.id.botoneditarperfil);
+        val ircatalogo = findViewById<ImageButton>(R.id.tarjetaGoCatalogo);
+        val irsubir = findViewById<ImageButton>(R.id.tarjetaGoSubida);
+        val irperfil = findViewById<ImageButton>(R.id.tarjetaGoPerfil);
 
-        home.setOnClickListener {
-            val intent = Intent(this@perfiluser, catalogogeneros::class.java)
+        ircatalogo.setOnClickListener {
+            val intent = Intent(this@tarjeta, catalogogeneros::class.java)
             startActivity(intent)
 
 
         }
-        subir.setOnClickListener {
-            val intent = Intent(this@perfiluser, subircancion::class.java)
+        irsubir.setOnClickListener {
+            val intent = Intent(this@tarjeta, subircancion::class.java)
             startActivity(intent)
 
 
         }
-
-        editperfil.setOnClickListener {
-            val intent = Intent(this@perfiluser, tarjeta::class.java)
+        irperfil.setOnClickListener {
+            val intent = Intent(this@tarjeta, perfiluser::class.java)
             startActivity(intent)
 
 
