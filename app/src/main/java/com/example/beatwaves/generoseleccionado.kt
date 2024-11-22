03e3a09e7b6eafd07d9b68c85642d811acc1a8b6
Ivan <97ivangarcia@gmail.com>
2024-11-22 17:38:57 +0100

    funcionalidad botones en la clase generoseleccionado

--- app/src/main/java/com/example/beatwaves/generoseleccionado.kt
+++ app/src/main/java/com/example/beatwaves/generoseleccionado.kt
@@ -3,6 +3,7 @@ package com.example.beatwaves
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,38 +19,30 @@ class generoseleccionado : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val botongenero1 = findViewById<Button>(R.id.generoboton1);
-        val botongenero2 = findViewById<Button>(R.id.generoboton2);
-        val botongenero3 = findViewById<Button>(R.id.generoboton3);
-        val botongenero4 = findViewById<Button>(R.id.generoboton4);
+
+        val generoirhome = findViewById<ImageButton>(R.id.generoGoHome);
+        val generoirsubida = findViewById<ImageButton>(R.id.generoGoSubir);
+        val generoirperfil = findViewById<ImageButton>(R.id.generoGoPerfil);
+
+        generoirhome.setOnClickListener {
+            val intent = Intent(this@generoseleccionado, catalogogeneros::class.java)
+            startActivity(intent)
 
 
-        //AQUI SE CONFIGURARÁN LOS BOTONES CUANDO CONECTEMOS LA BASE DE DATOS
-
-        botongenero1.setOnClickListener {
-            val intent = Intent(this@generoseleccionado, perfiluser::class.java)
+        }
+        generoirsubida.setOnClickListener {
+            val intent = Intent(this@generoseleccionado, subircancion::class.java)
             startActivity(intent)
 
 
         }
 
-        botongenero2.setOnClickListener {
-            val intent = Intent(this@generoseleccionado, perfiluser::class.java)
+        generoirperfil.setOnClickListener {
+            val intent = Intent(this@generoseleccionado, tarjeta::class.java)
             startActivity(intent)
 
 
         }
-        botongenero3.setOnClickListener {
-            val intent = Intent(this@generoseleccionado, perfiluser::class.java)
-            startActivity(intent)
 
-
-        }
-        botongenero4.setOnClickListener {
-            val intent = Intent(this@generoseleccionado, perfiluser::class.java)
-            startActivity(intent)
-
-
-        }
     }
 }
